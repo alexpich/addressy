f46e4f2f86c447e6606320dc37d285afe2bfaa5f
alexpich <pich.alex01@gmail.com>
2020-04-20 15:30:14 -0700

    Handle basic validation for fields

--- tests/Feature/ContactsTest.php
+++ tests/Feature/ContactsTest.php
@@ -15,8 +15,41 @@ class ContactsTest extends TestCase
     public function a_contact_can_be_added()
     {
         $this->withoutExceptionHandling();
-        $this->post('/api/contacts', ['name' => 'Test name']);
+        $this->post(
+            '/api/contacts',
+            $this->data()
+        );
 
-        $this->assertCount(1, Contact::all());
+        $contact = Contact::first();
+
+        $this->assertEquals('Test name', $contact->name);
+        $this->assertEquals('test@email.com', $contact->email);
+        $this->assertEquals('01/01/1990', $contact->birthday);
+        $this->assertEquals('Company Name', $contact->company);
+    }
+
+    /** @test */
+    public function fields_are_required()
+    {
+        collect(['name', 'email', 'birthday', 'company'])
+            ->each(function ($field) {
+                $response = $this->post(
+                    '/api/contacts',
+                    array_merge($this->data(), [$field => ''])
+                );
+
+                $response->assertSessionHasErrors($field);
+                $this->assertCount(0, Contact::all());
+            });
+    }
+
+    private function data()
+    {
+        return [
+            'name' => 'Test name',
+            'email' => 'test@email.com',
+            'birthday' => '01/01/1990',
+            'company' => 'Company Name'
+        ];
     }
 }
